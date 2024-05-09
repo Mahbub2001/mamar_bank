d75faf4415cd6878dba216feab7c0d0ae32cd411
Mahbub2001 <imahbubturza@gmail.com>
2024-05-09 14:21:30 +0600

    first commit

---
+++ mamar_bank/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from  core.views import HomeView
+urlpatterns = [
+    
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name = "homepage"),
+    path('accounts/', include('accounts.urls')),
+    path('transactions/', include('transactions.urls')),
+
+]
